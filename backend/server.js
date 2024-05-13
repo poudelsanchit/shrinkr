c119f122a55c7f38dd127ad675823d8810786264
Sanchit poudel <snchtpdl12@gmail.com>
2024-05-13 19:40:51 +0545

    build(deps): delete 1 file and update 3 files

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,25 @@ const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connect");
 const app = express();
 const cors = require("cors");
-const port = 5000;
-const {handleGetRedirectedUrl} = require('./controllers/url')
+const dotenv = require("dotenv");
+const { handleGetRedirectedUrl } = require("./controllers/url");
 const URL = require("./model/url");
 
-connectToMongoDB("mongodb://127.0.0.1:27017/urlShortner").then(() => {
+dotenv.config();
+connectToMongoDB(
+  "mongodb+srv://poudelsanchit:NoobMaster$96@cluster0.qayskoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/urlshortner"
+).then(() => {
   console.log("Mongodb Connected");
 });
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   const urls = await URL.find().exec();
 
   return res.json(urls);
 });
 app.use("/url", urlRoute);
 app.get("/:shortId", handleGetRedirectedUrl);
-app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server started at port ${process.env.PORT}`);
 });
